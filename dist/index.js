bcf92cb7788d2ce2dadada0e2c6dea81a7f72b43
Valiant John <valiant@junesoftware.com>
2021-08-08 19:29:42 +0530

    refactor: Changed eslint config - Code refactoring - Main test script changed

--- dist/index.js
+++ dist/index.js
@@ -42,41 +42,40 @@ exports.getAndroidTargetArchitectures = exports.getScriptingBackendName = export
 const core = __importStar(__nccwpck_require__(186));
 const fs_1 = __importDefault(__nccwpck_require__(747));
 const yaml_1 = __importDefault(__nccwpck_require__(552));
-const playerSettingsKey = "PlayerSettings";
-const scriptingBackendKey = "scriptingBackend";
-const scriptingDefineSymbolsKey = "scriptingDefineSymbols";
-const architectureKey = "architecture";
-const versionKey = "versionNo";
-const bundleVersionKey = "bundleVersion";
-const buildNumberKey = "buildNumber";
-const undefined = "Undefined";
+const playerSettingsKey = 'PlayerSettings';
+const scriptingBackendKey = 'scriptingBackend';
+const scriptingDefineSymbolsKey = 'scriptingDefineSymbols';
+const architectureKey = 'architecture';
+const versionKey = 'versionNo';
+const bundleVersionKey = 'bundleVersion';
+const buildNumberKey = 'buildNumber';
+const undefined = 'Undefined';
 function exportProperties(yamlObject, platform) {
-    core.info(yamlObject[playerSettingsKey].toString());
-    core.setOutput("playerSettings", yamlObject[playerSettingsKey].toString());
+    core.setOutput('playerSettings', yamlObject[playerSettingsKey].toString());
     switch (platform) {
-        case "Android":
-            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["7"].toString());
-            core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]["Android"]).toString());
-            core.setOutput(architectureKey, getAndroidTargetArchitectures(yamlObject[playerSettingsKey]["AndroidTargetArchitectures"]).toString());
-            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey]["AndroidBundleVersionCode"].toString()));
+        case 'Android':
+            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['7'].toString());
+            core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]['Android']).toString());
+            core.setOutput(architectureKey, getAndroidTargetArchitectures(yamlObject[playerSettingsKey]['AndroidTargetArchitectures']).toString());
+            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey]['AndroidBundleVersionCode'].toString()));
             break;
-        case "StandaloneWindows64":
-            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["1"].toString());
-            core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]["Standalone"]).toString());
-            core.setOutput(architectureKey, "64Bit");
-            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]["Standalone"].toString()));
+        case 'StandaloneWindows64':
+            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['1'].toString());
+            core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]['Standalone']).toString());
+            core.setOutput(architectureKey, '64Bit');
+            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]['Standalone'].toString()));
             break;
-        case "StandaloneLinux64":
-            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["1"].toString());
-            core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]["Standalone"]).toString());
-            core.setOutput(architectureKey, "64Bit");
-            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]["Standalone"].toString()));
+        case 'StandaloneLinux64':
+            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['1'].toString());
+            core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]['Standalone']).toString());
+            core.setOutput(architectureKey, '64Bit');
+            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]['Standalone'].toString()));
             break;
-        case "iOS":
-            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["4"].toString());
-            core.setOutput(scriptingBackendKey, "IL2CPP");
-            core.setOutput(architectureKey, "64Bit");
-            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]["iPhone"].toString()));
+        case 'iOS':
+            core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['4'].toString());
+            core.setOutput(scriptingBackendKey, 'IL2CPP');
+            core.setOutput(architectureKey, '64Bit');
+            core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]['iPhone'].toString()));
             break;
         default:
             core.setOutput(scriptingDefineSymbolsKey, undefined);
@@ -88,26 +87,26 @@ function exportProperties(yamlObject, platform) {
 }
 exports.exportProperties = exportProperties;
 function getVersionNo(version, build) {
-    const seperator = " #";
+    const seperator = ' #';
     return version.concat(seperator, build);
 }
 exports.getVersionNo = getVersionNo;
 function getScriptingBackendName(id) {
     if (id === 0)
-        return "Mono";
+        return 'Mono';
     else if (id === 1)
-        return "IL2CPP";
+        return 'IL2CPP';
     else
         return undefined;
 }
 exports.getScriptingBackendName = getScriptingBackendName;
 function getAndroidTargetArchitectures(id) {
     if (id === 1)
-        return "ARMv7";
+        return 'ARMv7';
     else if (id === 2)
-        return "ARM64";
+        return 'ARM64';
     else if (id === 3)
-        return "ARMv7, ARM64";
+        return 'ARMv7, ARM64';
     else
         return undefined;
 }
@@ -117,7 +116,7 @@ function run() {
         try {
             const platform = core.getInput('platform'); //Get input parameter from YAML file(Actions workflow file)
             const path = core.getInput('path'); //Get project settings file path
-            const yamlFile = fs_1.default.readFileSync(path, "utf8"); //Load the project settings file
+            const yamlFile = fs_1.default.readFileSync(path, 'utf8'); //Load the project settings file
             const yamlObject = yaml_1.default.parse(yamlFile); //Parse using YAML library
             exportProperties(yamlObject, platform);
         }
--- src/main.ts
+++ src/main.ts
@@ -1,87 +1,134 @@
 import * as core from '@actions/core'
-import fs from "fs";
-import yaml from "yaml";
+import fs from 'fs'
+import yaml from 'yaml'
 
-const playerSettingsKey = "PlayerSettings";
-const scriptingBackendKey = "scriptingBackend";
-const scriptingDefineSymbolsKey = "scriptingDefineSymbols";
-const architectureKey = "architecture";
-const versionKey = "versionNo";
-const bundleVersionKey = "bundleVersion";
-const buildNumberKey = "buildNumber";
-const undefined = "Undefined";
-
+const playerSettingsKey = 'PlayerSettings'
+const scriptingBackendKey = 'scriptingBackend'
+const scriptingDefineSymbolsKey = 'scriptingDefineSymbols'
+const architectureKey = 'architecture'
+const versionKey = 'versionNo'
+const bundleVersionKey = 'bundleVersion'
+const buildNumberKey = 'buildNumber'
+const undefined = 'Undefined'
 
 export function exportProperties(yamlObject: any, platform: string): void {
-  core.info(yamlObject[playerSettingsKey].toString());
-  core.setOutput("playerSettings", yamlObject[playerSettingsKey].toString());
+  core.setOutput('playerSettings', yamlObject[playerSettingsKey].toString())
 
   switch (platform) {
-    case "Android":
-      core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["7"].toString());
-      core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]["Android"]).toString());
-      core.setOutput(architectureKey, getAndroidTargetArchitectures(yamlObject[playerSettingsKey]["AndroidTargetArchitectures"]).toString());
-      core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey]["AndroidBundleVersionCode"].toString()));
-      break;
-    case "StandaloneWindows64":
-      core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["1"].toString());
-      core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]["Standalone"]).toString());
-      core.setOutput(architectureKey, "64Bit");
-      core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]["Standalone"].toString()));
-      break;
-    case "StandaloneLinux64":
-      core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["1"].toString());
-      core.setOutput(scriptingBackendKey, getScriptingBackendName(yamlObject[playerSettingsKey][scriptingBackendKey]["Standalone"]).toString());
-      core.setOutput(architectureKey, "64Bit");
-      core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]["Standalone"].toString()));
-      break;
-    case "iOS":
-      core.setOutput(scriptingDefineSymbolsKey, yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]["4"].toString());
-      core.setOutput(scriptingBackendKey, "IL2CPP");
-      core.setOutput(architectureKey, "64Bit");
-      core.setOutput(versionKey, getVersionNo(yamlObject[playerSettingsKey][bundleVersionKey].toString(), yamlObject[playerSettingsKey][buildNumberKey]["iPhone"].toString()));
-      break;
+    case 'Android':
+      core.setOutput(
+        scriptingDefineSymbolsKey,
+        yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['7'].toString()
+      )
+      core.setOutput(
+        scriptingBackendKey,
+        getScriptingBackendName(
+          yamlObject[playerSettingsKey][scriptingBackendKey]['Android']
+        ).toString()
+      )
+      core.setOutput(
+        architectureKey,
+        getAndroidTargetArchitectures(
+          yamlObject[playerSettingsKey]['AndroidTargetArchitectures']
+        ).toString()
+      )
+      core.setOutput(
+        versionKey,
+        getVersionNo(
+          yamlObject[playerSettingsKey][bundleVersionKey].toString(),
+          yamlObject[playerSettingsKey]['AndroidBundleVersionCode'].toString()
+        )
+      )
+      break
+    case 'StandaloneWindows64':
+      core.setOutput(
+        scriptingDefineSymbolsKey,
+        yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['1'].toString()
+      )
+      core.setOutput(
+        scriptingBackendKey,
+        getScriptingBackendName(
+          yamlObject[playerSettingsKey][scriptingBackendKey]['Standalone']
+        ).toString()
+      )
+      core.setOutput(architectureKey, '64Bit')
+      core.setOutput(
+        versionKey,
+        getVersionNo(
+          yamlObject[playerSettingsKey][bundleVersionKey].toString(),
+          yamlObject[playerSettingsKey][buildNumberKey]['Standalone'].toString()
+        )
+      )
+      break
+    case 'StandaloneLinux64':
+      core.setOutput(
+        scriptingDefineSymbolsKey,
+        yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['1'].toString()
+      )
+      core.setOutput(
+        scriptingBackendKey,
+        getScriptingBackendName(
+          yamlObject[playerSettingsKey][scriptingBackendKey]['Standalone']
+        ).toString()
+      )
+      core.setOutput(architectureKey, '64Bit')
+      core.setOutput(
+        versionKey,
+        getVersionNo(
+          yamlObject[playerSettingsKey][bundleVersionKey].toString(),
+          yamlObject[playerSettingsKey][buildNumberKey]['Standalone'].toString()
+        )
+      )
+      break
+    case 'iOS':
+      core.setOutput(
+        scriptingDefineSymbolsKey,
+        yamlObject[playerSettingsKey][scriptingDefineSymbolsKey]['4'].toString()
+      )
+      core.setOutput(scriptingBackendKey, 'IL2CPP')
+      core.setOutput(architectureKey, '64Bit')
+      core.setOutput(
+        versionKey,
+        getVersionNo(
+          yamlObject[playerSettingsKey][bundleVersionKey].toString(),
+          yamlObject[playerSettingsKey][buildNumberKey]['iPhone'].toString()
+        )
+      )
+      break
     default:
-      core.setOutput(scriptingDefineSymbolsKey, undefined);
-      core.setOutput(scriptingBackendKey, undefined);
-      core.setOutput(architectureKey, undefined);
+      core.setOutput(scriptingDefineSymbolsKey, undefined)
+      core.setOutput(scriptingBackendKey, undefined)
+      core.setOutput(architectureKey, undefined)
       core.setOutput(versionKey, undefined)
-      break;
+      break
   }
 }
 
-export function getVersionNo(version: string, build: string): string{
-  const seperator: string = " #";
-  return version.concat(seperator, build);
+export function getVersionNo(version: string, build: string): string {
+  const seperator = ' #'
+  return version.concat(seperator, build)
 }
 
 export function getScriptingBackendName(id: number): string {
-  if (id === 0)
-    return "Mono";
-  else if (id === 1)
-    return "IL2CPP";
-  else
-    return undefined;
+  if (id === 0) return 'Mono'
+  else if (id === 1) return 'IL2CPP'
+  else return undefined
 }
 
 export function getAndroidTargetArchitectures(id: number): string {
-  if (id === 1)
-    return "ARMv7";
-  else if (id === 2)
-    return "ARM64";
-  else if (id === 3)
-    return "ARMv7, ARM64";
-  else
-    return undefined;
+  if (id === 1) return 'ARMv7'
+  else if (id === 2) return 'ARM64'
+  else if (id === 3) return 'ARMv7, ARM64'
+  else return undefined
 }
 
 async function run(): Promise<void> {
   try {
-    const platform: string = core.getInput('platform'); //Get input parameter from YAML file(Actions workflow file)
-    const path: string = core.getInput('path'); //Get project settings file path
-    const yamlFile = fs.readFileSync(path, "utf8"); //Load the project settings file
-    const yamlObject = yaml.parse(yamlFile); //Parse using YAML library
-    exportProperties(yamlObject, platform);
+    const platform: string = core.getInput('platform') //Get input parameter from YAML file(Actions workflow file)
+    const path: string = core.getInput('path') //Get project settings file path
+    const yamlFile = fs.readFileSync(path, 'utf8') //Load the project settings file
+    const yamlObject = yaml.parse(yamlFile) //Parse using YAML library
+    exportProperties(yamlObject, platform)
   } catch (error) {
     core.setFailed(error.message)
   }
